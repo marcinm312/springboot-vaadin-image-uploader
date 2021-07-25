912bb490599fccaadea4b3313c40349da2568c1c
marcinm312 <marcin.michalczyk.312@gmail.com>
2021-07-26 00:18:43 +0200

    Image numbering improvement in GalleryGui

--- src/main/java/pl/marcinm312/springbootimageuploader/gui/GalleryGui.java
+++ src/main/java/pl/marcinm312/springbootimageuploader/gui/GalleryGui.java
@@ -2,11 +2,13 @@ package pl.marcinm312.springbootimageuploader.gui;
 
 import com.flowingcode.vaadin.addons.carousel.Carousel;
 import com.flowingcode.vaadin.addons.carousel.Slide;
+import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.StyleSheet;
 import com.vaadin.flow.component.html.Anchor;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
@@ -38,6 +40,11 @@ public class GalleryGui extends VerticalLayout {
 	Button prevImageButton;
 	Button lastImageButton;
 	Button firstImageButton;
+	Span paginationSpan;
+	Text paginationText;
+
+	private final transient List<ImageDto> allImagesFromDB;
+	private int imageNumber = 1;
 
 	private final transient org.slf4j.Logger log = LoggerFactory.getLogger(getClass());
 
@@ -58,32 +65,60 @@ public class GalleryGui extends VerticalLayout {
 		h1 = new H1("Image gallery");
 
 		log.info("Loading all images from DB");
-		List<ImageDto> allImagesFromDB = imageService.getAllImagesFromDB();
+		allImagesFromDB = imageService.getAllImagesFromDB();
 		log.info("allImagesFromDB.size()={}", allImagesFromDB.size());
 
-		prepareImageCarousel(allImagesFromDB);
-		prepareNavigationButtons(allImagesFromDB);
+		prepareImageCarousel();
+		prepareNavigationButtons();
 
 		add(horizontalMenu, h1, carousel, navigationButtons);
 	}
 
-	private void prepareNavigationButtons(List<ImageDto> allImagesFromDB) {
+	private void prepareNavigationButtons() {
 		nextImageButton = new Button(">>");
 		prevImageButton = new Button("<<");
 		lastImageButton = new Button(">|");
 		firstImageButton = new Button("|<");
-		nextImageButton.addClickListener(e -> carousel.moveNext());
-		prevImageButton.addClickListener(e -> carousel.movePrev());
-		lastImageButton.addClickListener(e -> carousel.movePos(allImagesFromDB.size() - 1));
-		firstImageButton.addClickListener(e -> carousel.movePos(0));
-		navigationButtons = new HorizontalLayout(firstImageButton, prevImageButton, nextImageButton, lastImageButton);
+		paginationText = new Text(preparePaginationText());
+		nextImageButton.addClickListener(e -> {
+			if (imageNumber == allImagesFromDB.size()) {
+				imageNumber = 1;
+			} else {
+				imageNumber++;
+			}
+			paginationText.setText(preparePaginationText());
+			carousel.moveNext();
+		});
+		prevImageButton.addClickListener(e -> {
+			if (imageNumber == 1) {
+				imageNumber = allImagesFromDB.size();
+			} else {
+				imageNumber--;
+			}
+			paginationText.setText(preparePaginationText());
+			carousel.movePrev();
+		});
+		lastImageButton.addClickListener(e -> {
+			imageNumber = allImagesFromDB.size();
+			paginationText.setText(preparePaginationText());
+			carousel.movePos(allImagesFromDB.size() - 1);
+		});
+		firstImageButton.addClickListener(e -> {
+			imageNumber = 1;
+			paginationText.setText(preparePaginationText());
+			carousel.movePos(0);
+		});
+		paginationSpan = new Span();
+		paginationSpan.add(paginationText);
+		navigationButtons = new HorizontalLayout(firstImageButton, prevImageButton, paginationSpan, nextImageButton,
+				lastImageButton);
 		navigationButtons.setAlignItems(Alignment.CENTER);
 		navigationButtons.setJustifyContentMode(JustifyContentMode.CENTER);
 		navigationButtons.setWidthFull();
 		navigationButtons.setMargin(false);
 	}
 
-	private void prepareImageCarousel(List<ImageDto> allImagesFromDB) {
+	private void prepareImageCarousel() {
 		carousel = new Carousel().withoutNavigation();
 		List<Slide> slidesList = new ArrayList<>();
 		for (ImageDto imageDto : allImagesFromDB) {
@@ -113,5 +148,9 @@ public class GalleryGui extends VerticalLayout {
 	boolean isUserAdmin() {
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 		return "ROLE_ADMIN".equals(((AppUser) authentication.getPrincipal()).getRole());
+	}
+
+	private String preparePaginationText() {
+		return "Image " + imageNumber + " of " + allImagesFromDB.size();
 	}
 }
